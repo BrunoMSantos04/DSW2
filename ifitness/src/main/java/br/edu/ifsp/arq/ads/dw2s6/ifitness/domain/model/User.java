7d07e119edb67d7e614572cf112be877162a925e
Bruno Santos <marcilio.santos@aluno.ifsp.edu.br>
2024-11-22 10:29:57 -0300

    Atualizado

---
+++ ifitness/src/main/java/br/edu/ifsp/arq/ads/dw2s6/ifitness/domain/model/User.java
@@ -0,0 +1,135 @@
+package br.edu.ifsp.arq.ads.dw2s6.ifitness.domain.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String name;
+	@NotNull
+	@Email
+	private String email;
+	@NotNull
+	private String password;
+	@NotNull
+	@Column(name = "birth_date")
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dateOfBirth;
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+	@NotNull
+	private Boolean active;
+	@ManyToMany(fetch = FetchType.EAGER) // fetch = buscar - eager = ancioso
+	@JoinTable(name = "user_permission", joinColumns = @JoinColumn(name = "id_user"), 
+	inverseJoinColumns = @JoinColumn(name = "id_permission"))
+	private List<Permission> permissions;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public Boolean isActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ ifitness/src/main/java/br/edu/ifsp/arq/ads/dw2s6/ifitness/service/ActivityService.java
@@ -0,0 +1,44 @@
+package br.edu.ifsp.arq.ads.dw2s6.ifitness.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifsp.arq.ads.dw2s6.ifitness.domain.model.Activity;
+import br.edu.ifsp.arq.ads.dw2s6.ifitness.domain.model.User;
+import br.edu.ifsp.arq.ads.dw2s6.ifitness.repository.ActivityRepository;
+import br.edu.ifsp.arq.ads.dw2s6.ifitness.repository.UserRepository;
+import br.edu.ifsp.arq.ads.dw2s6.ifitness.service.exception.NonExistentOrInactiveUserException;
+
+@Service
+public class ActivityService {
+	
+	@Autowired
+	private ActivityRepository activityRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Activity save(Activity activity) {
+		Optional<User> user = userRepository.findById(activity.getUser().getId());
+		if(!user.isPresent() || !user.get().isActive()) {
+			throw new NonExistentOrInactiveUserException();
+		}
+		return activityRepository.save(activity);
+	}
+	
+	public Activity update(Long id, Activity activity) {
+		Activity activitySaved = findActivityById(id);
+		BeanUtils.copyProperties(activity, activitySaved, "id");
+		return activityRepository.save(activitySaved);
+	}
+	
+	public Activity findActivityById(Long id) {
+		Activity activitySaved = activityRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return activitySaved;
+	}
+
+}
